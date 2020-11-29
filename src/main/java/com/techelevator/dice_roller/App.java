c00668da595d965312cf0dc1a5e9283ddb1389f2
Sean Hunt <seanthunt@gmail.com>
2020-11-29 14:48:32 -0500

    Add everything

---
+++ src/main/java/com/techelevator/dice_roller/App.java
@@ -0,0 +1,20 @@
+package com.techelevator.dice_roller;
+
+import java.util.Scanner;
+
+public class App {
+
+	public Scanner input = new Scanner(System.in);
+	
+	public void main(String[] args) {
+		
+	}
+	
+	public int roll() {
+		int output = 0;
+		
+		
+		
+		return output;
+	}
+}
